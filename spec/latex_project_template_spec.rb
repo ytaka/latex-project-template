c9b53ca13ea0d7b6c3215e5ad95a9ea93604d6a8
Takayuki YAMAGUCHI <d@ytak.info>
2011-07-07 16:34:50 +0900

    Add specs.

---
+++ spec/latex_project_template_spec.rb
@@ -0,0 +1,62 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe LaTeXProjectTemplate::Configuration do
+  before(:all) do
+    @home_directory = File.join(File.dirname(__FILE__), 'test_home')
+    @home_directory_absolute = File.expand_path(@home_directory)
+  end
+
+  subject do
+    LaTeXProjectTemplate::Configuration.new(@home_directory)
+  end
+
+  it "should create directory." do
+    LaTeXProjectTemplate::Configuration.create_new_config(@home_directory)
+    File.directory?(@home_directory).should be_true
+  end
+
+  it "should return config directory." do
+    subject.config_directory.should == File.join(@home_directory_absolute, '.latex_project_template')
+  end
+
+  it "should list template." do
+    list = subject.list_template
+    list.should be_an_instance_of Array
+    list.should include('default')
+  end
+
+  it "should return path of template." do
+    subject.template_exist?('default').should be_an_instance_of LaTeXProjectTemplate::Directory
+  end
+
+  it "should return false" do
+    subject.template_exist?('not_exist').should be_false
+  end
+
+  after(:all) do
+    FileUtils.rm_r(@home_directory)
+  end
+
+end
+
+describe LaTeXProjectTemplate do
+  before(:all) do
+    @home_directory = File.join(File.dirname(__FILE__), 'test_home')
+    @home_directory_absolute = File.expand_path(@home_directory)
+    LaTeXProjectTemplate::Configuration.create_new_config(@home_directory)
+  end
+
+  it "should copy 'default' template." do
+    template = LaTeXProjectTemplate.new(File.join(@home_directory, 'new_tex'), 'default', @home_directory)
+    template.create
+  end
+
+  it "should copy 'article' template." do
+    template = LaTeXProjectTemplate.new(File.join(@home_directory, 'new_article'), 'article', @home_directory)
+    template.create
+  end
+
+  after(:all) do
+    FileUtils.rm_r(@home_directory)
+  end
+end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,11 +4,9 @@ describe LaTeXProjectTemplate::Task do
   it "should return default value" do
     LaTeXProjectTemplate::Task.new("sample.tex") do |task|
       task.latexmk.path.should == 'latexmk'
-      [:dvi, :ps, :pdf, :clean, :distclean].each do |sym|
+      [:dvi, :ps, :pdf, :pdfdvi, :pdfps, :clean, :distclean].each do |sym|
         task.latexmk.command(sym, "sample.tex").should be_an_instance_of String
       end
     end
   end
 end
-
-
