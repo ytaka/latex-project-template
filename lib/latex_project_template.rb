8c2395a330490195c4e54cd2af6491053573ce7a
Takayuki YAMAGUCHI <d@ytak.info>
2011-07-07 19:27:12 +0900

    Output created files when copying template.

--- lib/latex_project_template.rb
+++ lib/latex_project_template.rb
@@ -55,8 +55,11 @@ class LaTeXProjectTemplate
   end
 
   class Directory
+    attr_reader :path, :name
+
     def initialize(path)
       @path = File.expand_path(path)
+      @name = File.basename(@path)
     end
 
     def create_directory_if_needed(target_dir)
@@ -71,7 +74,7 @@ class LaTeXProjectTemplate
     private :create_directory_if_needed
 
     def target_path(project_name, target_dir, target_name)
-      target_name = target_name.gsub(/__DOT__/, '/.')
+      target_name = target_name.gsub(/__DOT__/, '.')
       target_name = target_name.gsub(/__PROJECT__/, project_name)
       File.join(target_dir, target_name.sub(/^#{Regexp.escape(@path)}/, ''))
     end
@@ -79,40 +82,49 @@ class LaTeXProjectTemplate
 
     # Create file of which name is created by removing '.erb' from name of original file
     def create_erb_template(erb_file, erb_obj, project_name, target_dir)
-      erb_obj.instance_exec(erb_file, target_path(project_name, target_dir, erb_file.sub(/\.erb$/, ''))) do |path, out|
+      product_path = target_path(project_name, target_dir, erb_file.sub(/\.erb$/, ''))
+      erb_obj.instance_exec(erb_file, product_path) do |path, out|
         erb = ERB.new(File.read(path))
         open(out, 'w') do |f|
           f.print erb.result(binding)
         end
       end
+      product_path
     end
     private :create_erb_template
 
-    IGNORE_FILE_REGEXP = /\/(__IMPORT__$|__IGNORE__)/
+    IGNORE_FILE_REGEXP = /(\/__IMPORT__$|\/__IGNORE__|~$)/
 
     def copy_to_directory(target_dir, erb_binding_obj, files = nil)
       create_directory_if_needed(target_dir)
       if files
         file_list = files.map do |file_path|
-          File.join(@path, file_path)
-        end
+          fullpath = File.join(@path, file_path)
+          if File.exist?(fullpath)
+            fullpath
+          else
+            nil
+          end
+        end.compact
       else
         file_list = Dir.glob(File.join(@path, '**', '*')).sort
       end
+      created_files = []
       file_list.each do |file|
         next if IGNORE_FILE_REGEXP =~ file
         create_directory_if_needed(File.dirname(file))
-        if File.directory?(file)
-          FileUtils.mkdir_p(file)
-        else
+        unless File.directory?(file)
           case file
           when /\.erb$/
-            create_erb_template(file, erb_binding_obj, erb_binding_obj.project_name, target_dir)
+            created = create_erb_template(file, erb_binding_obj, erb_binding_obj.project_name, target_dir)
           else
-            FileUtils.cp(file, target_path(erb_binding_obj.project_name, target_dir, file))
+            created = target_path(erb_binding_obj.project_name, target_dir, file)
+            FileUtils.cp(file, created)
           end
+          created_files << created
         end
       end
+      created_files
     end
 
     def files_to_import
@@ -153,20 +165,29 @@ class LaTeXProjectTemplate
 
   def create_files
     erb_obj = LaTeXProjectTemplate::ErbObject.new(@project_name)
-    @template.copy_to_directory(@target_dir, erb_obj)
+    created_files = @template.copy_to_directory(@target_dir, erb_obj)
     @template.files_to_import.each do |name, files|
       if template_to_import = @config.template_exist?(name)
-        template_to_import.copy_to_directory(@target_dir, erb_obj, files)
+        created_files.concat(template_to_import.copy_to_directory(@target_dir, erb_obj, files))
       end
     end
+    created_files.sort!
   end
   private :create_files
 
-  def create
+  def create(opts = {})
     FileUtils.mkdir_p(@target_dir)
     git = Git.init(@target_dir)
-    create_files
+    files = create_files
     git.add
-    git.commit("Initial commit.")
+    git.commit("Copy template '#{@template.name}'.")
+    if io = opts[:io]
+      files.map! do |path|
+        path.sub!(@target_dir, '')
+        path.sub!(/^\//, '')
+      end
+      io.puts "Create the following files from template '#{@template.name}' and commit to git."
+      io.puts files.join("\n")
+    end
   end
 end
